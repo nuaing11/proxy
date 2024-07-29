4813932941b15095aaa06757429627a5f1aab142
nuaing11 aja <poconuaing@gmail.com>
2024-07-29 05:56:41 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x7DBd2F8A7eAF85a949a2721714f0cC32706B6728'
+export const ProxyAdmin = '0x1f6461f2C763b85bd17Bcf91A8eF7419918f1eB1'
+export const SWTRImplementation = '0x5bD1201594a74D9dDC2bC1f64da9316e88850e8d'
     
